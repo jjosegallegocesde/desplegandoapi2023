bd707684ce50db4d0433c9ec1e1d5db0a6f2f78b
julian588 <agu.sdvsf@gmail.com>
2023-11-10 06:19:11 -0500

    ok

---
+++ Api.js
@@ -0,0 +1,26 @@
+import express from 'express'
+
+
+import {rutas} from './routes/rutas.js'
+import {establecerConexion} from './database/conexion.js'
+
+export class Api {
+    constructor() {
+        this.app = express()
+        this.conectarBD()
+        this.procesarPeticiones()
+    }
+    leventarServidor() {
+        this.app.listen(3000, function () {
+            console.log("servidor operando con exito")
+        })
+    }
+    procesarPeticiones() {
+        this.app.use(express.json())
+        this.app.use('/',rutas)
+       
+    }
+    conectarBD() { 
+        establecerConexion()
+    }
+}
